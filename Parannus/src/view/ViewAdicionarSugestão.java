8ad5a1a5b37cf6de49972f3b2d2c599eb09f65fe
Hugo Rebelo <hugdiniz@gmail.com>
2015-07-07 10:49:52 -0300

    Second Commit

---
+++ Parannus/src/view/ViewAdicionarSugestão.java
@@ -0,0 +1,11 @@
+package view;
+
+public class ViewAdicionarSugestão {
+	public ViewAdicionarSugestão() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		System.out.println("Oi");
+	}
+
+}

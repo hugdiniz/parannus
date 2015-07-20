2db9216e6a11cfe1dd20f6332e4d057ccc8818fb
Hugo Rebelo <hugdiniz@gmail.com>
2015-07-20 16:50:04 -0300

    teste Funcional

--- Parannus/src/teste/unitario/TesteUnitario.java
+++ Parannus/src/teste/unitario/TesteUnitario.java
@@ -1,5 +1,15 @@
 package teste.unitario;
 
+import org.dbunit.Assertion;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.filter.DefaultColumnFilter;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.Before;
+
 public class TesteUnitario
 {
 
